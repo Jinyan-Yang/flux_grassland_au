bfcb5da4aa1dc05b61f038586a582a9b0a6c4115
Jinyan Yang <jyyoung@live.com>
2021-09-15 12:10:33 +1000

    main function to call model and plots

---
+++ main.R
@@ -0,0 +1,11 @@
+# prepare th environment####
+source('r/functions_gday.R')
+
+#### run gday for given sites
+source('r/test_gday.R')
+
+# run gday for differnt sites####
+source('r/run_gday_site.R')
+
+# make plot####
+source('r/model_evaluation.R')
